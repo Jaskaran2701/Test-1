285e8ccfd3459bd9cdc4faf7e35e5619262b0963
Jaskaran2701 <jaskaran.nugen@gmail.com>
2024-09-13 13:09:16 +0530

    fixed/text-issue

--- src/app/dashboard/card.tsx
+++ src/app/dashboard/card.tsx
@@ -5,7 +5,7 @@ type CardProps={
 
 export default function Card({value,label}:CardProps){
     return(
-    <div className="bg-white w-32 h-32 xl:w-52 xl:h-52 rounded-3xl flex flex-col justify-center items-center gap-4 hover:cursor-pointer hover:bg-blue-600 hover:text-white hover:scale-110 transition ease-in-out delay-150">
+    <div className="bg-white text-black w-32 h-32 xl:w-52 xl:h-52 rounded-3xl flex flex-col justify-center items-center gap-4 hover:cursor-pointer hover:bg-blue-600 hover:text-white hover:scale-110 transition ease-in-out delay-150">
         <div className="text-6xl xl:text-8xl">{value}</div>
         <div className="text-xs xl:text-sm capitalize">{label}</div>
       </div>
